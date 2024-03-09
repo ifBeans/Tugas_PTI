f867bc680b80b2b3d3074dd21bf6fdc7e02ac4d1
KnoWingFly <112180149+KnoWingFly@users.noreply.github.com>
2024-03-09 18:32:19 +0700

    Tabel udh bisa, sesuai ama data

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-alert("test")
+var data = [
+    {"nim": "0000000001", "nama": "admin", "alamat": "system"},
+];
+
+$(document).ready(function(){
+    var tbody = $('#bodytable');
+    
+    data.forEach(item => {
+        var row = `<tr>
+                       <td>${item.nim}</td>
+                       <td>${item.nama}</td>
+                       <td>${item.alamat}</td>
+                       <td><button type="button" class="btn btn-primary">Ubah</button>  <button type="button" class="btn btn-danger">Hapus</button></td>
+                   </tr>`;
+        tbody.append(row);
+    });
+})
